5f71cc87d61bbaf69d80651c3cd058cb743f72d1
Sara Cemal <saracemal@gmail.com>
2020-10-22 02:37:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,7 +56,7 @@ def game_hash
             blocks: 11,
             slam_dunks: 1
           }
-      },
+      }
     away: {
         team_name: "Charlotte Hornets",
         colors: ["turqoise", "purple"],
