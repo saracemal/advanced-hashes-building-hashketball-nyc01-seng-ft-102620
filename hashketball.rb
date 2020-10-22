0475d63a597df3cd45a768bde9cc4a0f5203de95
Sara Cemal <saracemal@gmail.com>
2020-10-22 01:58:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,15 +5,15 @@ def game_hash
         team name: "Brooklyn Nets",
         colors: ["black", "white"],
         players: {
-          "Alan Anderson",
-          :number => "0",
-          :shoe => "16",
-          :points => "22",
-          :rebounds => "12",
-          :assists => "12",
-          :steals => "3",
-          :blocks => "1",
-          :slam_dunks => "1"]
+          "Alan Anderson" => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1]
           [
             ]
           
