ecbcffbc27316cbdd532a8ee021591f4b3b6fe29
Sara Cemal <saracemal@gmail.com>
2020-10-22 01:50:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,12 +6,33 @@ def hashketball
       {
         :team_name => "Brooklyn Nets",
         :colors => ["black", "white"],
-        :players =>
+        :players => {
+          
+          :player_name => "Alan Anderson",
+          :number => "0",
+          :shoe => "16",
+          :points => "22",
+          :rebounds => "12",
+          :assists => "12",
+          :steals => "3",
+          :blocks => "1",
+          :slam_dunks => "1"]
+        }
       },
     :away => 
       {
         :team_name => "Charlotte Hornets",
         :colors => ["turqoise", "purple"],
-        :players =>
+        :players => [
+          :player_name =>,
+          :number =>,
+          :shoe =>,
+          :points =>,
+          :rebounds =>,
+          :assists =>,
+          :steals =>,
+          :blocks =>,
+          :slam_dunks
+          ]
       }
   }
