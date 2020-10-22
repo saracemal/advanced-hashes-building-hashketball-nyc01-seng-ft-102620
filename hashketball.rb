34de4f3dad9a8c3d0b59f2ed7a7e401fef2e471e
Sara Cemal <saracemal@gmail.com>
2020-10-22 02:11:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,22 +14,101 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
-        }
+        },
+          "Reggie Evans" => {
+            number: 30,
+            shoe: 14,
+            points: 12,
+            rebounds: 12,
+            assists: 12,
+            steals: 12,
+            blocks: 12,
+            slam dunks: 7
+        },
+          "Brook Lopez" => {
+            number: 11,
+            shoe: 17,
+            points: 17,
+            rebounds: 19,
+            assists: 12,
+            steals: 3,
+            blocks: 1,
+            slam dunks: 15
+        },
+          "Mason Plumlee" => {
+            number: 1,
+            shoe: 19,
+            points: 26,
+            rebounds: 11,
+            assists: 6,
+            steals: 3,
+            blocks: 8,
+            slam dunks: 5
+        },
+          "Jason Terry" => {
+            number: 31,
+            shoe: 15,
+            points: 19, 
+            rebounds: 2,
+            assists: 2,
+            steals: 4,
+            blocks: 11,
+            slam dunks: 1
+          }
       },
-    :away => 
-      {
-        :team_name => "Charlotte Hornets",
-        :colors => ["turqoise", "purple"],
-        :players => [
-          :player_name =>,
-          :number =>,
-          :shoe =>,
-          :points =>,
-          :rebounds =>,
-          :assists =>,
-          :steals =>,
-          :blocks =>,
-          :slam_dunks
-          ]
+    away: {
+        team name: "Charlotte Hornets",
+        colors: ["turqoise", "purple"],
+        players: {
+          "Jeff Adrian" => {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+       },
+          "Bismack Biyombo" => {
+            number: 0,
+            shoe: 16,
+            points: 12,
+            rebounds: 4,
+            assists: 7,
+            steals: 22,
+            blocks: 15,
+            slam dunks: 10
+        },
+          "DeSagna Diop" => {
+            number: 2,
+            shoe: 14, 
+            points: 24,
+            rebounds: 12, 
+            assists: 12, 
+            steals: 4, 
+            blocks: 5,
+            slam dunks, 5
+        },
+          "Ben Gordon" => {
+            number: 8, 
+            shoe: 15,
+            points: 33, 
+            rebounds: 3,
+            assists: 2,
+            steals: 1, 
+            blocks: 1,
+            slam dunks: 0
+        },
+          "Kemba Walker" => {
+            number: 33,
+            shoe: 15,
+            points: 6,
+            rebounds: 12,
+            assists: 12,
+            steals: 7,
+            blocks: 5, 
+            slam dunks: 12
+        },
       }
   }
